d9f3e9164dc52209d519c72e71858691acc37204
Ales <ales.kavcic86@gmail.com>
2020-05-10 11:48:34 +0200

    Add parcelable for serialization and remove unecessary code

--- app/src/main/java/com/example/kiviapp/datamodel/Vehicle.kt
+++ app/src/main/java/com/example/kiviapp/datamodel/Vehicle.kt
@@ -1,20 +1,43 @@
 package com.example.kiviapp.datamodel
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class Vehicle(
-    /*val userId: Int,
-    val id: Int,
-    val title: String,
-    val body: String*/
     @SerializedName("brand")
-    val brand: String,
+    val brand: String?,
     @SerializedName("model")
-    val model: String,
+    val model: String?,
     @SerializedName("type")
-    val type: VehicleType
+    val type: VehicleType?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()?.let { VehicleType.valueOf(it) }
+    )
 
-)
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(brand)
+        parcel.writeString(model)
+        parcel.writeString(type?.name);
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Vehicle> {
+        override fun createFromParcel(parcel: Parcel): Vehicle {
+            return Vehicle(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Vehicle?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
 
 
 
