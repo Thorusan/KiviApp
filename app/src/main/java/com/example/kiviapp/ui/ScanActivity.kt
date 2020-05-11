695deacb1a04f4d25f2932d9ca39c117025089f2
Ales <ales.kavcic86@gmail.com>
2020-05-11 18:26:43 +0200

    Added activity for sound

--- app/src/main/java/com/example/kiviapp/ui/ScanActivity.kt
+++ app/src/main/java/com/example/kiviapp/ui/ScanActivity.kt
@@ -1,57 +1,13 @@
 package com.example.kiviapp.ui
 
-import android.media.MediaPlayer
-import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.example.kiviapp.R
-import kotlinx.coroutines.*
-
 
 class ScanActivity : AppCompatActivity() {
-    var player: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
-
-        scanQRCode()
     }
-
-    private fun scanQRCode() {
-        GlobalScope.launch(context = Dispatchers.Main) {
-            delay(500)
-            runBlocking {
-                play()
-                delay(5000)
-                player!!.isLooping = false
-                finish()
-            }
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        stopPlayer()
-    }
-
-    fun play() {
-        if (player == null) {
-            player = MediaPlayer.create(this, R.raw.zvok)
-            player!!.setOnCompletionListener { stopPlayer() }
-
-        }
-        player!!.start()
-        player!!.isLooping = true
-    }
-
-    private fun stopPlayer() {
-        if (player != null) {
-            player!!.release()
-            player = null
-        }
-    }
-
-
 }
---
+++ app/src/main/java/com/example/kiviapp/ui/SoundActivity.kt
@@ -0,0 +1,55 @@
+package com.example.kiviapp.ui
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kiviapp.R
+import kotlinx.coroutines.*
+
+
+class SoundActivity : AppCompatActivity() {
+    var player: MediaPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sound)
+
+        playSound()
+    }
+
+    private fun playSound() {
+        GlobalScope.launch(context = Dispatchers.Main) {
+            delay(500)
+            runBlocking {
+                play()
+                delay(5000)
+                player!!.isLooping = false
+                finish()
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        stopPlayer()
+    }
+
+    fun play() {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.zvok)
+            player!!.setOnCompletionListener { stopPlayer() }
+
+        }
+        player!!.start()
+        player!!.isLooping = true
+    }
+
+    private fun stopPlayer() {
+        if (player != null) {
+            player!!.release()
+            player = null
+        }
+    }
+
+
+}
