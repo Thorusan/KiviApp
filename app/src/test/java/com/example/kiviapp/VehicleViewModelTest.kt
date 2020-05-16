9bcf5fda33c3cc1af051cd832b7a3adba62c6459
Ales <ales.kavcic86@gmail.com>
2020-05-16 15:05:06 +0200

    updated test

--- app/src/test/java/com/example/kiviapp/VehicleViewModelTest.kt
+++ app/src/test/java/com/example/kiviapp/VehicleViewModelTest.kt
@@ -3,9 +3,9 @@ package com.example.kiviapp
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import com.example.kiviapp.datamodel.Vehicle
+import com.example.kiviapp.datamodel.VehicleType
 import com.example.kiviapp.repository.VehicleRepository
 import com.example.kiviapp.viewmodel.Resource
-import com.example.kiviapp.viewmodel.Status
 import com.example.kiviapp.viewmodel.VehicleViewModel
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.timeout
@@ -13,7 +13,6 @@ import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.*
 import kotlinx.coroutines.test.setMain
-
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -24,10 +23,15 @@ import org.junit.runners.JUnit4
 class VehicleViewModelTest {
     private lateinit var viewModel: VehicleViewModel
     private lateinit var vehicleRepository: VehicleRepository
-    private val successResource = Resource.success(ArrayList<Vehicle>())
-    private val errorResource = Resource.error("Unauthorised", null)
-    private lateinit var vehicleObserver: Observer<Resource<List<Vehicle>>>
 
+    private val vehicleList = listOf(
+        Vehicle("Audi", "A3", VehicleType.CAR),
+        Vehicle("Audi", "A4", VehicleType.CAR),
+        Vehicle("Audi", "A8", VehicleType.CAR)
+    )
+    private val successResource = Resource.success(vehicleList)
+
+    private lateinit var vehicleObserver: Observer<Resource<List<Vehicle>>>
 
     @Rule
     @JvmField
@@ -43,18 +47,17 @@ class VehicleViewModelTest {
         Dispatchers.setMain(mainThreadSurrogate)
         vehicleRepository = mock()
         runBlocking {
-            vehicleRepository.getVehicles()
+            whenever(vehicleRepository.getVehicles()).thenReturn(successResource)
         }
         viewModel = VehicleViewModel(vehicleRepository)
         vehicleObserver = mock()
     }
 
     @Test
-    fun `when getVehicles is called, then observer is updated with success`() = runBlocking {
-        viewModel.loadVehicleData().observeForever(vehicleObserver)
+    fun `when loadVehicleData is called, then observer is updated with success`() = runBlocking {
+        //viewModel.loadVehicleData().observeForever(vehicleObserver)
         delay(10)
         verify(vehicleObserver, timeout(50)).onChanged(Resource.loading(null))
+        verify(vehicleObserver, timeout(50)).onChanged(successResource)
     }
-
-
 }
